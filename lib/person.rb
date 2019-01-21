d7cf1b61869658a813a8560fd4547f25db715a6e
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-21 20:48:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,4 +3,7 @@ class Person
   attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight,
                 :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_width,
                 :pant_length
+
+    def initialize(attributes)
+
 end
